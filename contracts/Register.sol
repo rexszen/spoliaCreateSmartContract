20a0025c50bd8a08f0a3b79ce02fceb88ac3d934
Zenexa <kzenuar@gmail.com>
2023-06-23 15:46:40 +0700

    Spolia Smart Contract

---
+++ contracts/Register.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "Zenyaha";
+        owner = 0x24282BD697B33C0288Ca5b48CfA81D87c702E7f8;
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
